00fa686cbfd84f703c659e6c16f39d2999231d6c
Abhishek Sai <Abhishek-Sai>
2018-02-27 23:24:43 +0530

    Last few

---
+++ Source Codes Stable/fibonacci using recursions.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int fib(int x)
+{
+    if(x==0||x==1)
+    {
+        return x;
+    }
+    else
+    {
+        return (fib(x-1)+fib(x-2));
+    }
+}
+void main()
+{
+	int third,n,i;
+	printf("Enter the limit\n");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		third=fib(i);
+		printf("%d\t",third);
+	}
+}

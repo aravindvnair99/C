93a02dbefd29abcd7df23cd946e8d62f3cd89ef6
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-02-27 21:08:28 +0530

    Add

--- Source Codes Testing/CSE17004/div.c
+++ Source Codes Testing/CSE17004/div.c
@@ -1,34 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+
 void main()
 {
-
 	int x;
-	printf("Enter the nuimber \n");
-	scanf("%d",&x);
-	if(x==0)
-	{
-	printf("Neither Even nor Odd \n");
-	}
+	printf("Enter the number: \n");
+	scanf("%d", &x);
+	if(x == 0)
+		printf("Neither Even nor Odd \n");
 	else
-	{
-		
-		if(x%2==0)
 		{
-		printf("Even number");
-			if(x%4==0)
-			{
-				printf("Divisible by 4");
-			}
+			
+			if(x%2==0)
+				{
+					printf("Even number");
+					if(x%4==0)
+						printf("Divisible by 4");
+					else	
+						printf("Not Divisible by 4");
+				}
 			else
-			{
-					
-				printf("Not Divisible by 4");
-			}
+				printf("Odd Number");
 		}
-		else
-		{
-		printf("Odd Number");
-		}
-	}
 
 }

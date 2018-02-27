7dc9fa7c793ea9e5a67d4bf06306013ea65dfb20
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-02-27 21:15:55 +0530

    Add

---
+++ Source Codes Stable/Even or odd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+	{
+		int n;
+		printf("Enter a number:\n");
+		scanf("%d", &n);
+		if(n % 2 == 0)
+			{
+				printf("%d is a even number.\n", n);
+			}
+		else
+			{
+				printf("%d is a odd number.\n", n);
+			}
+		
+	}
+	

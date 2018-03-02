c3ee9d1735ef76098ae53b6863e647a2b6b3b54d
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-03-02 11:55:04 +0530

    Update Hash pattern.c

--- Source Codes Stable/Hash pattern.c
+++ Source Codes Stable/Hash pattern.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 
-int main()
-{
-	int i,j;
-	for(i=0;i<=4;i++)
-	{
-		for(j=1;j<=i;j++)
-		{
-		printf("# \t");
-		}
-	printf("\n");
-	}
-}
-
+void main()
+    {
+	    int i, j, n;
+    	printf("Enter number of lines:\n");
+    	scanf("%d", &n);
+    	for(i=0; i<=n; i++)
+        	{
+        		for(j=1; j<=i; j++)
+            		{
+            		    printf("#\t");
+            		}
+        	    printf("\n");
+        	}
+    }

617dba963645047fa35128b1ec34efcdd7ffa32f
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-03-08 14:04:21 +0530

    2D Array

--- Source Codes Stable/2D Array.c
+++ Source Codes Stable/2D Array.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-	void main() {
-		int s, n, i, j, r, c;
-		printf("Enter the number of rows \n");
-		scanf("%d", &r);
-		printf("Enter the number of columns \n");
-		scanf("%d", &c);
-		int a[r][c];
-		for (i = 0; i < r; i++) {
-			for (j = 0; j < c; j++) {
-				scanf("%d", &a[i][j]);
-			}
-		}
-		for (i = 0; i < r; i++) {
-			for (j = 0; j < c; j++) {
-				printf("%d\t", a[i][j]);
-			}
-			printf("\n");
-		}
-	}
+
+void main() {
+    int s, n, i, j, r, c;
+    printf("Enter the number of rows:\n");
+    scanf("%d", &r);
+    printf("Enter the number of columns:\n");
+    scanf("%d", &c);
+    int a[r][c];
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            printf("Enter the array element for %d, %d:\n", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("The array is:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+}

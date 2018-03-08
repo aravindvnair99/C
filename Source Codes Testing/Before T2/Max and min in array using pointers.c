74f2cf62dce64f850b12380068ca39718e7b3eb0
Abhishek Sai <Abhishek-Sai>
2018-03-08 13:52:48 +0530

    Pointer in new folder

---
+++ Source Codes Testing/Before T2/Max and min in array using pointers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+	int n,*pa,i,max,min;
+	printf("Enter array size\n");;
+	scanf("%d",&n);
+	int a[n];
+	pa=&a[0];
+	printf("Enter array elements\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",(pa+i));
+	}	
+	max=*pa;
+	min=*pa;
+	for(i=0;i<n;i++)
+	{
+		if(*(pa+i)>max)
+		{
+			max=*(pa+i);
+		}
+		if(*(pa+i)<min)
+		{
+			min=*(pa+i);
+		}
+	}
+	printf("The maximum number is %d\n",max);
+	printf("The minimum number is %d\n",min);
+}

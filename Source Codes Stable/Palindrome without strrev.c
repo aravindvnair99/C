aef8084f1720bfeb11c5c1d286700550eb425f15
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-03-27 09:41:54 +0000

    Create Palindrome without strrev.c

---
+++ Source Codes Stable/Palindrome without strrev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+	{
+		int len, i;
+		char s1[100];
+		printf("Enter a word: \n");
+		scanf("%s", s1);
+		len = strlen(s1);
+		char s2[len];
+		for (i=0; s1[i] != '\0'; i++)
+			{
+				s2[len-i-1] = s1[i];
+			}
+		s2[i+1] = '\0';
+		if (strcmp(s1,s2) == 0)
+			printf("Palindrome.\n");
+		else
+			printf("Not a palindrome.\n");
+	}

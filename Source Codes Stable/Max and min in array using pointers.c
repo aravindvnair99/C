81f418aa4f059adc2ccd959c2c3aefc01a287e62
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-04-03 00:29:40 +0530

    Update and rename Source Codes Testing/Before T2/Max and min in array using pointers.c to Source Codes Stable/Max and min in array using pointers.c

---
+++ Source Codes Stable/Max and min in array using pointers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+    {
+    	int n, *pa, i, max, min;
+    	printf("Enter array size: ");;
+    	scanf("%d", &n);
+    	int a[n];
+    	pa = &a[0];
+    	printf("Enter array elements:\n");
+    	for(i=0; i<n; i++)
+    		scanf("%d", (pa+i));
+    	max = *pa;
+    	min = *pa;
+    	for(i=0;i<n;i++)
+        	{
+        		if(*(pa+i)>max)
+        			max = *(pa+i);
+        		if(*(pa+i)<min)
+        			min = *(pa+i);
+        	}
+    	printf("The minimum number is %d.\n", min);
+    	printf("The maximum number is %d.\n", max);
+    }

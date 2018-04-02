f49d840dc80c3b6917956a5aa71979afbdf0cf2b
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-04-03 00:23:47 +0530

    Create Check if entered character is an alphabet or not.c

---
+++ Source Codes Stable/Check if entered character is an alphabet or not.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+    {						
+        char c;
+        printf("Enter a character: ");
+        scanf("%c", &c);
+        if( (c>='a' && c<='z') || (c>='A' && c<='Z') )
+            printf("%c is an alphabet.", c);
+        else
+            printf("%c is not an alphabet.", c);
+    }

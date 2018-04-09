80baa3b7d8331ed583656ccf1645c8313d528b0d
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-04-09 21:42:35 +0530

    Code refactoring

--- Source Codes Stable/Pointer to a structure.c
+++ Source Codes Stable/Pointer to a structure.c
@@ -2,22 +2,22 @@
 
 struct student
 {
-    char name[50];
-    char branch[15];
+	char name[50];
+	char branch[15];
 };
 
 int main()
 {
-    int n, i;
-    struct student s[n];
-    struct student *ptr;
-    ptr = &s[i];
-    printf("Enter student's first name: ");
-    scanf("%s", ptr->name);
-    printf("Enter student branch: ");
-    scanf("%s", ptr->branch);
-    printf("Enter student's new branch: ");
-    scanf("%s", ptr->branch);
-    printf("Student's new branch: %s\n", ptr->branch);
-    return 0;
+	int n, i;
+	struct student s[n];
+	struct student *ptr;
+	ptr = &s[i];
+	printf("Enter student's first name: ");
+	scanf("%s", ptr->name);
+	printf("Enter student branch: ");
+	scanf("%s", ptr->branch);
+	printf("Enter student's new branch: ");
+	scanf("%s", ptr->branch);
+	printf("Student's new branch: %s\n", ptr->branch);
+	return 0;
 }

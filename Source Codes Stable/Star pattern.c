25afdc5b93362427e4317fe163b85d24b0d282dd
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-04-10 20:20:47 +0530

    Flush

--- Source Codes Stable/Star pattern.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-void main()
-	{
-		printf("\t \t * \n");
-		printf("\t * \t \t * \n");
-		printf("* \t \t * \t \t *  \n");
-	}
--- Source Codes Stable/Sum of two numbers.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-void main()
-{
-	int a, b;
-	printf("Enter a and b:\n");
-	scanf("%d %d", &a, &b);
-	printf("Sum of %d and %d = %d\n", a, b, a + b);
-}

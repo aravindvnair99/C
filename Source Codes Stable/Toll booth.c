76e89e880ae92c74f059882f751449fc06309b1b
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-04-20 14:19:01 +0530

    Refactor

--- Source Codes Stable/Toll booth.c
+++ Source Codes Stable/Toll booth.c
@@ -2,83 +2,65 @@
 
 char tr;
 
-void car()
-
-	{
-		printf("Enter the type of trip:\n");
-		printf("Enter 's' for single and 'm' for multiple:\n");
-		scanf(" %c", &tr);
-		switch(tr)
-			{
-			case 's':	printf("The cost of ticket is 55.\n");
-						break;
-			case 'm':
-						printf("The cost is 80.\n");
-						break;
-			default:
-						printf("Invalid input.\n");
-						break;
-			}	
+void car()	{
+	printf("Enter the type of trip:\n");
+	printf("Enter 's' for single and 'm' for multiple:\n");
+	scanf(" %c", &tr);
+	switch(tr)	{
+		case 's':	printf("The cost of ticket is 55.\n");
+					break;
+		case 'm':
+					printf("The cost is 80.\n");
+					break;
+		default:
+					printf("Invalid input.\n");
+					break;
 	}
-
-void bus_truck()
-
-	{
-		printf("Enter the type of trip:\n");
-			printf("Enter 's' for single and 'm' for multiple:\n");
-			scanf(" %c",&tr);
-			switch(tr)
-			{
-				case 's':
-				printf("The cost of ticket is 170.\n");
-				break;
-				case 'm':
-				printf("The cost is 255.\n");
-				break;
-				default:
-				printf("Invalid input.\n");
-				break;
-			}
-	}
-
-void contain()
-
-	{
-		printf("Enter the type of trip:\n");
-			printf("Enter 's' for single and 'm' for multiple:\n");
-			scanf(" %c",&tr);
-			switch(tr)
-			{
-				case 's':	printf("The cost of ticket is 255");
-							break;
-				case 'm':	printf("The cost is 385");
-							break;
-				default:	printf("Invalid input.\n");
-							break;
-			}
-	}
-
-void LCV()
-{
-	printf("Enter the type of trip \n");
-		printf("Enter s for single \n m for multiple");
-		scanf(" %c",&tr);
-		switch(tr)
-		{
-			case 's':
-			printf("The cost of ticket is 85");
-			break;
-			case 'm':
-			printf("The cost is 125");
-			break;
-			default:
-			printf("Invalid input.\n");
-			break;
-		}
 }
 
-void main()
-{
+void bus_truck()	{
+	printf("Enter the type of trip:\n");
+	printf("Enter 's' for single and 'm' for multiple:\n");
+	scanf(" %c", &tr);
+	switch(tr)	{
+		case 's':	printf("The cost of ticket is 170.\n");
+					break;
+		case 'm':	printf("The cost is 255.\n");
+					break;	
+		default:	printf("Invalid input.\n");
+					break;
+	}
+}
+
+void contain()	{
+	printf("Enter the type of trip:\n");
+	printf("Enter 's' for single and 'm' for multiple:\n");
+	scanf(" %c", &tr);
+	switch(tr)	{
+		case 's':	printf("The cost of ticket is 255");
+					break;
+		case 'm':	printf("The cost is 385");
+					break;
+		default:	printf("Invalid input.\n");
+					break;
+	}
+}
+
+void LCV()	{
+	printf("Enter the type of trip \n");
+	printf("Enter s for single \n m for multiple");
+	scanf(" %c", &tr);
+	switch(tr)	{
+		case 's':	printf("The cost of ticket is 85");
+					break;
+		case 'm':	printf("The cost is 125");
+					break;
+		default:	printf("Invalid input.\n");
+					break;
+	}
+}
+
+void main()	{
 	char k, tr;
 	printf("Enter the type of vehicle: \n");
 	printf("Enter c for car.\n");
@@ -86,9 +68,8 @@ void main()
 	printf("Enter t for truck.\n");
 	printf("Enter T for containment truck.\n");
 	printf("Enter L for LCV.\n");
-	scanf("%c",&k);
-	switch(k)
-	{
+	scanf("%c", &k);
+	switch(k)	{
 		case 'c':	car();
 					break;
 		case 't':

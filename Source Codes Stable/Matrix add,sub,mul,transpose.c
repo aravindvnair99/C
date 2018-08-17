aa64d107bc92b7f00a9463b4e67c095423d41027
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-08-17 17:04:55 +0530

    Update Matrix add,sub,mul,transpose.c

--- Source Codes Stable/Matrix add,sub,mul,transpose.c
+++ Source Codes Stable/Matrix add,sub,mul,transpose.c
@@ -1,162 +1,121 @@
-#include<stdio.h>
-void main()
-{
-	int s,n,i,j,r1,c1,r2,c2;
+#include <stdio.h>
+
+void main() {
+	int i, j, r1, c1, r2, c2;
 	printf("Enter the number of rows for 1st matrix \n");
-	scanf("%d",&r1);
+	scanf("%d", &r1);
 	printf("Enter the number of columns for 1st matrix \n");
-	scanf("%d",&c1);
+	scanf("%d", &c1);
 	int a[r1][c1];
-	for(i=0;i<r1;i++)
-	{
-		for(j=0;j<c1;j++)
-		{
+	for (i=0; i<r1; i++) {
+		for (j=0; j<c1; j++) {
 			scanf("%d",&a[i][j]);
 		}
-		
 	}
 	printf("Enter the number of rows for 2nd matrix \n");
-	scanf("%d",&r2);
+	scanf("%d", &r2);
 	printf("Enter the number of columns for 2nd matrix \n");
-	scanf("%d",&c2);
+	scanf("%d", &c2);
 	int b[r2][c2];
-	for(i=0;i<r2;i++)
-	{
-		for(j=0;j<c2;j++)
-		{
-			scanf("%d",&b[i][j]);
+	for (i=0; i<r2; i++) {
+		for (j=0; j<c2; j++) {
+			scanf("%d", &b[i][j]);
 		}
-		
 	}
 	int c[r1][c1];
-	if(r1==r2 && c1==c2)
-	{
-		for(i=0;i<r1;i++)
-		{
+	if (r1==r2 && c1==c2) {
+		for (i=0; i<r1; i++) {
 			for(j=0;j<c1;j++)
 			{
 				c[i][j]=a[i][j]+b[i][j];
 			}
 		}
 		printf("The addition of the matrices are \n");
-		for(i=0;i<r1;i++)
-		{
-			for(j=0;j<c1;j++)
-			{
+		for (i=0; i<r1; i++) {
+			for (j=0; j<c1; j++) {
 				printf("%d\t",c[i][j]);
 			}
 			printf("\n");
 		}
 	}
-	else
-	{
+	else {
 		printf("Dimensions are wrong! \n");
 	}
 	
 	int d[r1][c1];
-	if(r1==r2 && c1==c2)
-	{
-		for(i=0;i<r1;i++)
-		{
-			for(j=0;j<c1;j++)
-			{
+	if (r1==r2 && c1==c2) {
+		for(i=0;i<r1;i++) {
+			for(j=0;j<c1;j++) {
 				d[i][j]=a[i][j]-b[i][j];
 			}
 		}
 		printf("\nThe subtraction of the matrices are \n");
-		for(i=0;i<r1;i++)
-		{
-			for(j=0;j<c1;j++)
-			{
+		for(i=0;i<r1;i++) {
+			for(j=0;j<c1;j++) {
 				printf("%d\t",d[i][j]);
 			}
 			printf("\n");
 		}
 	}
-	else
-	{
+	else {
 		printf("Dimensions are wrong! \n");
 	}
-	
 	int m[r1][c2];
 	int k;
-	if(c1==r2)
-	{
-		for(i=0;i<r1;i++)
-		{
-			for(j=0;j<c2;j++)
-			{
-				for(k=0;k<c1;k++)
-				{
+	if (c1==r2) {
+		for(i=0;i<r1;i++) {
+			for(j=0;j<c2;j++) {
+				for(k=0;k<c1;k++) {
 					m[i][j]=0;
 					m[i][j]=m[i][j]+(a[i][k]*b[k][j]);
 				}
 			}
 		}
 		printf("\nThe multiplication of the matrices are \n");
-		for(i=0;i<r1;i++)
-		{
-			for(j=0;j<c2;j++)
-			{
+		for (i=0; i<r1; i++) {
+			for(j=0; j<c2; j++) {
 				printf("%d\t",m[i][j]);
 			}
 			printf("\n");
 		}
 	}
-	else
-	{
+	else {
 		printf("Matrix multiplication is not possible\n");
 	}
 	int at[r1][c1];
-	if(r1==c1)
-	{
-		for(i=0;i<r1;i++)
-		{
-			for(j=0;j<c1;j++)
-			{
-				at[j][i]=a[i][j];
+	if (r1==c1) {
+		for (i=0; i<r1; i++) {
+			for (j=0; j<c1; j++) {
+				at[j][i] = a[i][j];
 			}
 		}
 		printf("The transpose of A is \n");
-		for(i=0;i<r1;i++)
-		{
-			for(j=0;j<c1;j++)
-			{
+		for (i=0; i<r1; i++) {
+			for (j=0; j<c1; j++) {
 				printf("%d\t",at[i][j]);
 			}
 			printf("\n");
 		}
 	}
-	else
-	{
+	else {
 		printf("The transpose of A is not possible \n");
 	}
 	int bt[r2][c2];
-	if(r2==c2)
-	{
-		for(i=0;i<r2;i++)
-		{
-			for(j=0;j<c2;j++)
-			{
+	if (r2==c2) {
+		for (i=0;i<r2;i++) {
+			for(j=0;j<c2;j++) {
 				bt[j][i]=b[i][j];
 			}
 		}
 		printf("The transpose of B is \n");
-		for(i=0;i<r2;i++)
-		{
-			for(j=0;j<c2;j++)
-			{
+		for(i=0;i<r2;i++) {
+			for(j=0;j<c2;j++) {
 				printf("%d\t",bt[i][j]);
 			}
 			printf("\n");
 		}
 	}
-	else
-	{
-		printf("The transpose of B is not possible \n");
+	else {
+		printf("The transpose of B is not possible.\n");
 	}
-				
-	
 }
-	
-	

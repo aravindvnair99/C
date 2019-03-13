34148a68beaea87b182926e16a5655ab5be15ae5
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-03-13 11:06:45 +0530

    Update and rename Source Codes Stable/2D Array.c to Source Codes/2D Array.c

--- Source Codes/2D Array.c
+++ Source Codes/2D Array.c
@@ -2,22 +2,27 @@
 
 #include <stdio.h>
 
-void main() {
+void main()
+{
 	int i, j, r, c;
 	printf("Enter the number of rows:\n");
 	scanf("%d", &r);
 	printf("Enter the number of columns:\n");
 	scanf("%d", &c);
 	int a[r][c];
-	for (i = 0; i < r; i++) {
-		for (j = 0; j < c; j++) {
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < c; j++)
+		{
 			printf("Enter the array element for %d, %d:\n", i, j);
 			scanf("%d", &a[i][j]);
 		}
 	}
 	printf("The array is:\n");
-	for (i = 0; i < r; i++) {
-		for (j = 0; j < c; j++) {
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < c; j++)
+		{
 			printf("%d\t", a[i][j]);
 		}
 		printf("\n");

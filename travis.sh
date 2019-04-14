a795c90d44f38361aa8a1e84a1223b80ef307e99
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-04-14 19:19:16 +0530

    Path fix

--- travis.sh
+++ travis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find *.c -type f -print0 | while IFS= read -r -d $'\0' file; do
+find "Source Codes"/*.c -type f -print0 | while IFS= read -r -d $'\0' file; do
 	cp "$file" ./temp/"$file"
 done

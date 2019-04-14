e338814b343a598c39a46a0617a56f12151d11ac
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-04-14 19:40:09 +0530

    Travis

---
+++ Source Codes/travis.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+find *.c -type f -print0 | while IFS= read -r -d $'\0' file; do
+	cp "$file" "./temp/$file"
+done
--- travis.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find "Source Codes"/*.c -type f -print0 | while IFS= read -r -d $'\0' file; do
-	cp "$file" "../temp/$file"
-done

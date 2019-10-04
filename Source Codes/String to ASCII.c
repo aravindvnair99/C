aa2c2dacc2d68f3741b19a3528a9039ff17b787a
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-10-05 01:01:03 +0530

    Add code

---
+++ Source Codes/String to ASCII.c
@@ -0,0 +1,23 @@
+/*
+	 * C Program to Input a String & Store their Ascii Values in an Integer Array & Print the Array
+	 */
+
+#include <stdio.h>
+void main()
+{
+
+	char string[20];
+	int n, count = 0;
+
+	printf("Enter the no of characters present in an array \n ");
+	scanf("%d", &n);
+
+	printf(" Enter the string of %d characters \n", n);
+	scanf("%s", string);
+
+	while (count < n)
+	{
+		printf(" %c = %d\n", string[count], string[count]);
+		++count;
+	}
+}

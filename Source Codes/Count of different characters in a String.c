50b6366a2a414819889f971517e3e9ec98110e72
kaazima <59416370+kaazima@users.noreply.github.com>
2020-01-12 23:35:13 +0530

    Update and rename Different characters in a String.c to Count of different characters in a String.c
    C program to find the number of different characters in a string
    
    Signed-off-by: kaazima <kaazima.ifrah29@gmail.com>

--- Source Codes/Count of different characters in a String.c
+++ Source Codes/Count of different characters in a String.c
@@ -5,11 +5,12 @@ int main()
     char c[50],f[256]={0};          // since 256 ASCII characters
     int count=0;
     printf("Enter a string\n");
-    gets(c);
+    scanf("%[^\n]%*c",c);
     for(int i=0;c[i]!='\0';i++)
         f[c[i]]++;
     for(int i=0;i<256;i++)
         if(f[i]!=0)
             count++;
     printf("Number of different types of characters in entered string is %d",count);
+    return 0;
 }

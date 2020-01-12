58fd3530dcc219f168ef44c73db4b22030b842c7
kaazima <kaazima.ifrah29@gmail.com>
2020-01-12 22:39:44 +0530

    Added a file
    Signed-off-by: kaazima <kaazima.ifrah29@gmail.com>

---
+++ Source Codes/Different characters in a String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char c[50],f[256]={0};          // since 256 ASCII characters
+    int count=0;
+    printf("Enter a string\n");
+    gets(c);
+    for(int i=0;c[i]!='\0';i++)
+        f[c[i]]++;
+    for(int i=0;i<256;i++)
+        if(f[i]!=0)
+            count++;
+    printf("Number of different types of characters in entered string is %d",count);
+}

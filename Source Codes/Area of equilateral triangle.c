3c4552f10c791a8ce8fe588c5bfbaf2ae62c9b49
Priya378 <59410993+Priya378@users.noreply.github.com>
2020-01-14 00:21:40 +0530

    Changed indentation from spaces 4 to tabs 4
    Signed-off-by: Priya <bhuvanasree378@gmail.com>

--- Source Codes/Area of equilateral triangle.c
+++ Source Codes/Area of equilateral triangle.c
@@ -2,11 +2,11 @@
 #include<math.h>
 int main()
 {
-    int s;
-    float a;
-    printf("Enter side length of equilateral triangle ");
-    scanf("%d",&s);
-    a=(sqrt(3)*s*s)/4;
-    printf("Area of triangle is %.2f",a);
-    return 0;
+	int s;
+	float a;
+	printf("Enter side length of equilateral triangle ");
+	scanf("%d",&s);
+	a=(sqrt(3)*s*s)/4;
+	printf("Area of triangle is %.2f",a);
+	return 0;
 }

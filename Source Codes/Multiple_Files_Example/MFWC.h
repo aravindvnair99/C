bfe0869ead85a98f1ac7ca4a72a774062438d2dc
tkatemb <drxpnotlox@gmail.com>
2021-11-15 09:52:49 -0500

    Add new folder 'Multiple_Files_Example' to 'Source Codes' directory, teaches users about header files in C

---
+++ Source Codes/Multiple_Files_Example/MFWC.h
@@ -0,0 +1,8 @@
+//Header Guard (Prevents duplication of definitions)
+#ifndef MFWC_H
+#define MFWC_H
+
+//Declaration of function(s)
+void greetingFunction(char* userName);
+
+#endif
---
+++ Source Codes/Multiple_Files_Example/mainMFWC.c
@@ -0,0 +1,22 @@
+/*
+Good practice in C is to separate main(), functions declarations/structs, and function definitions, into different files.
+For the example, I will use MFWC.c and MFWC.h. (MFWC = "Multiple Files with C")
+~ tkatemb
+*/
+
+#include <stdio.h>
+#include "MFWC.h" //Include your header file for reference to definitions
+
+int main (void) {
+    char userName[20];
+
+    printf("Hello, please enter your name: \n");
+    scanf("%s", userName);
+
+    greetingFunction(userName);
+
+    return 0;
+    
+}
+
+

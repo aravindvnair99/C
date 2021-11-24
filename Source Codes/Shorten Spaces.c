9394dff8d2155da32fb142fe092b6d61a7234a95
tkatemb <drxpnotlox@gmail.com>
2021-11-24 16:49:23 -0500

    Updated

--- Source Codes/Shorten Spaces.c
+++ Source Codes/Shorten Spaces.c
@@ -1,12 +1,13 @@
 /*
 This removes duplicate spaces in text given from user using for loops and arrays.
 Follow comments in code!
-~tkatemb
+~trev
 */
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
+int main(void) 
+{
     //Declares string to store user input
     char userInput[500]; 
     //Prompt for user input
@@ -17,25 +18,29 @@ int main(void) {
     //Declare and initialize count variable for copying characters from "userInput" to "newText"
     int j = 0; 
     //Navigates through userInput string
-    for (int i = 0; i < strlen(userInput); i++) {
+    for (int i = 0; i < strnlen(userInput, 500); i++) 
+    {
         //If the current index in "userInput" is a SPACE *with a character in front of it*, copy the character to "newText"
-        if (userInput[i] == ' ' && userInput[i + 1] != ' ') {
+        if (userInput[i] == ' ' && userInput[i + 1] != ' ') 
+        {
             newText[j] = userInput[i];
             j++;
         }
         //If the current index in "userInput" is a NON-SPACE CHARACTER, copy the character to "newText"
-        else if (userInput[i] != ' ') {
+        else if (userInput[i] != ' ') 
+        {
             newText[j] = userInput[i];
             j++;
         }
         //If the current index in "userInput" is a SPACE *followed by another space*, do NOT copy to "newText"
-        else {
+        else 
+        {
             continue;
         }
         
     }
     //Appends null character to the end of the "newText" string.
-    newText[strlen(newText)] = '\0';
+    newText[strnlen(newText, 500)] = '\0';
     //Outputs the edited text
     printf("Edited text: %s", newText);
     
